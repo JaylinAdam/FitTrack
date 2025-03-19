090b97abe8497b80faa5b3a1042babacb83c72e4
Sean Nguyen <someseananigans@gmail.com>
2025-03-18 20:02:02 -0700

    [WJ-33] Introduce Async Storage functionality (#10)
    Co-authored-by: Sean Nguyen <sean.nguyen@glidewelldental.com>

--- WorkoutJournal2.0/src/Context/AppContext.tsx
+++ WorkoutJournal2.0/src/Context/AppContext.tsx
@@ -4,8 +4,10 @@ import React, {
     ReactNode,
     useMemo,
     useContext,
+    useEffect,
 } from 'react';
 import { Session, Exercise } from '../../models';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 interface AppState {
     sessions: Session[];
@@ -58,6 +60,7 @@ const AppProvider = ({ children }: { children: ReactNode }) => {
     const [sessions, setSessions] = useState<Session[]>([]);
     const [selected, setSelected] = useState('');
     const [visible, setVisible] = useState(false);
+    const [storageLoading, setStorageLoading] = useState(true);
 
     // global variable
     const currentDate = () => formatDate(new Date());
@@ -66,20 +69,23 @@ const AppProvider = ({ children }: { children: ReactNode }) => {
     const targetDate = () => (hasValue(selected) ? selected : CURRENT_DATE);
     const TARGET_DATE = targetDate();
 
-    // methods
+    // MEMO: targeted session
     const targetSession = useMemo(() => {
         const focusIndex = sessions?.findIndex(s => s.date === TARGET_DATE);
         return focusIndex !== -1 ? sessions[focusIndex] : undefined;
     }, [sessions, selected]);
 
+    // MEMO: today's session
     const todaySession = useMemo(() => {
         const focusIndex = sessions?.findIndex(s => s.date === CURRENT_DATE);
         return focusIndex !== -1 ? sessions[focusIndex] : undefined;
     }, [sessions, selected]);
 
+    // METHOD: Handle submission of exercise to session state
     const handleExSubmit = (exercise: Exercise) => {
         const sessionIndex = sessions?.findIndex(s => s.date === TARGET_DATE);
 
+        // if session exists
         if (sessionIndex >= 0) {
             const sessionList = [...sessions];
             sessionList[sessionIndex].exercises = [
@@ -87,13 +93,45 @@ const AppProvider = ({ children }: { children: ReactNode }) => {
                 exercise,
             ];
             setSessions(sessionList);
+            saveData(sessionList);
         } else {
             const newSession = new Session('', TARGET_DATE, '', [exercise]);
-            setSessions([...sessions, newSession]);
+            const sessionList = [...sessions, newSession];
+            setSessions(sessionList);
+            saveData(sessionList);
         }
 
         setVisible(false);
     };
+
+    // METHOD: Save data to local storage as "SessionData" (can be viewed in Application > Local Storage > localhost)
+    const saveData = async (newData: Session[]) => {
+        try {
+            await AsyncStorage.setItem('SessionData', JSON.stringify(newData));
+        } catch (error) {
+            console.error('Error saving data:', error);
+        }
+    };
+
+    // ONLOAD: check for and load session data (if existing)
+    useEffect(() => {
+        // METHOD: Load data from local storage under "SessionData" and set it to session state
+        const loadData = async () => {
+            try {
+                const storedData = await AsyncStorage.getItem('SessionData');
+                if (storedData) {
+                    const parseData = JSON.parse(storedData);
+                    setSessions(parseData);
+                    console.log('Stored Session Data', parseData);
+                }
+            } catch (error) {
+                console.error('Error loading data:', error);
+            } finally {
+                setStorageLoading(false);
+            }
+        };
+        loadData();
+    }, []);
 
     return (
         <AppContext.Provider
