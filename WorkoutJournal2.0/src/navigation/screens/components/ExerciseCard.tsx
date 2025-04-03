16354c48b45611699632013c3d5a4298bdf395dc
JaylinAdam <adamkhainguyen@gmail.com>
2025-04-02 19:22:00 -0700

    implemented options for card

--- WorkoutJournal2.0/src/navigation/screens/components/ExerciseCard.tsx
+++ WorkoutJournal2.0/src/navigation/screens/components/ExerciseCard.tsx
@@ -1,6 +1,6 @@
 import React, { useMemo, useState } from 'react';
 import { Text, StyleSheet, Pressable, Button, View } from 'react-native';
-import { Theme, Tools, useTheme } from '../../../Context';
+import { Theme, Tools, useApp, useTheme } from '../../../Context';
 
 interface Props {
     key: string;
@@ -8,8 +8,10 @@ interface Props {
     sets: string;
     reps: string;
     info: string;
+    index: number;
     onInsertPress: () => void;
     onDeletePress: () => void;
+    handleOptionsPress: () => void;
 }
 
 export const ExerciseCard = ({
@@ -17,38 +19,44 @@ export const ExerciseCard = ({
     sets,
     reps,
     info,
+    index,
     onInsertPress,
     onDeletePress,
+    handleOptionsPress,
 }: Props) => {
     // CONTEXTS
+    const { visible, options, selectedIndex } = useApp();
     const { theme } = useTheme();
     // STYLE
-    const styles = useMemo(() => createStyles(theme), [theme]);
-    // STATES
-    const [options, setOptions] = useState(false);
+    const styles = useMemo(() => createStyles(theme, options, index, selectedIndex, visible), [theme, options, index, selectedIndex, visible]);
 
     return (
-        <Pressable style={styles.note}>
-            <Text style={styles.title}>{name}</Text>
-            <Text style={styles.desc}>
-                {Tools.generateWorkoutDisplay(sets, reps, info)}
-            </Text>
-            <View style={[{ zIndex: options ? 1 : -1 }, StyleSheet.absoluteFillObject]}>
-                <Button onPress={onInsertPress} title='Edit'/>
-                <Button onPress={onDeletePress} title='Delete'/>
-            </View>
-        </Pressable>
+        <View style={styles.note}>
+            <Pressable style={styles.optionsWrapper} onPress={handleOptionsPress}>
+                <Text style={styles.title}>{name}</Text>
+                <Text style={styles.desc}>
+                    {Tools.generateWorkoutDisplay(sets, reps, info)}
+                </Text>
+                <View style={styles.options}>
+                    <Button onPress={onInsertPress} title='Edit'/>
+                    <Button onPress={onDeletePress} title='Delete'/>
+                </View>
+            </Pressable>
+        </View>
     );
 };
 
-const createStyles = (theme: Theme) =>
+const createStyles = (theme: Theme, options: boolean, index: number, selectedIndex: number, visible: boolean) =>
     StyleSheet.create({
         note: {
             padding: 8,
             borderRadius: 10,
             marginTop: 10,
+            zIndex: 1,
             boxShadow: '0px 0px 3px #a3a3a3',
             width: '100%',
+            borderWidth: 1,
+            borderColor: (options && index === selectedIndex && !visible) ? theme.background.accent : 'transparent',
         },
         title: {
             fontSize: 20,
@@ -57,5 +65,13 @@ const createStyles = (theme: Theme) =>
         desc: {
             fontSize: 16,
             color: theme.text.secondary,
+        },
+        optionsWrapper: { width: '100%' },
+        options: {
+            position: 'absolute',
+            right: 0,
+            flexDirection: 'row',
+            gap: 5,
+            display: (options && index === selectedIndex && !visible) ? 'flex' : 'none',
         },
     });
